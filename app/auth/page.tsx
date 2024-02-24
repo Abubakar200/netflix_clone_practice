2c0b47fd5c0ecd95c30660180c050958d93b6925
abubakar200 <sheikhabubakr4@gmail.com>
2024-02-24 11:52:48 +0500

    initial

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+    return ( 
+        <div className="relative h-full w-full bg-no-repeat bg-fixed bg-center bg-cover" style={{backgroundImage: "url('/images/hero.jpg')"}}>
+
+        </div>
+     );
+}
+ 
+export default Auth;
